f63269c0a3bbe0b7fe2dd74213ee30565e24324e
tungpt-vnist <55820228+tungpt-vnist@users.noreply.github.com>
2019-10-03 11:39:24 +0700

    Add files via upload

--- readFileSys.py
+++ readFileSys.py
@@ -1,10 +1,15 @@
 import csv
 import os
-user={
-    "name:":'',
-    "userDate:":'',
-    "Activity:":''
-}
+import json
+class User:
+    def __init__(self,name,userDate,activity):
+        self.name = name
+        self.userDate =userDate
+        self.activity = activity
+    def showUser(self):
+        print(self.name)
+
+useruse=[]
 def getUser(string):
     c= string.replace('"',"'")
     a= c.split(" ",4)
@@ -17,17 +22,37 @@ def getUser(string):
 def readFileSys(path):
     if os.path.isfile(path):
         with open(path,mode='r',encoding='utf-8') as file:
-            read = file.readline()
-            print(read)
-            a=getUser(read)
-            user["name:"]=a[2]
-            date=a[1]+"-"+a[0]
-            user["userDate:"]=date
-            user["Activity:"]=a[3]
-            user["name:"]='tung'
+            i=1
+            max = 0
+            readline=file.read()
+            userf = getUser(readline)
+            date = userf[1]+"-"+userf[0]
+            user1 =User(userf[2],date,userf[3])
+            useruse.append(user1)
+            #read = file.readline()
+            #print(read)
+            #a=getUser(read)
+            #date = a[1] + "-" + a[0]
+            #User1 =User(a[2],date,a[3])
+            #User2 =User("tung","21/03","coder")
+            #useruse.append(User1)
+            #useruse.append(User2)
+            for line in file:
+                info = getUser(line)
+                date1 = info[1]+'-'+info[0]
+                user="user"+ str(i)
+                user=User(info[2],date1,info[3])
+                if user.name !=info[2]:
+                    i = i + 1
+                    user =User(info[2],date1,info[3])
+                    useruse.append(user)
+                max = max+1
+                if(max==2000):
+                    break
 
     else:
         return False
 readFileSys("/home/tungkthd01/coypLogfile/syslog.1")
-for x,y in user.items():
-    print(x,y)
+print(len(useruse))
+for i in useruse:
+    i.showUser()
